f202092a37a54d5f822a9638a513c9a99a4f0ef7
Teszáry Péter <peterteszary@gmail.com>
2022-09-30 13:27:34 +0200

    First step

---
+++ wp-json.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP-JSON
+ * Plugin URI: https://purplepie.co
+ * Author: Peter Teszáry
+ * Author URI: https://peterteszary.com
+ * Description: This is a simple JSON reader plugin
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: opac-api
+ */
+
+ defined( 'ABSPATH') or die;
+
+ 
